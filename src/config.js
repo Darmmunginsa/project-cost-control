c8a9adc6d485f4a3be5f2b3b30b0d63519c9c390
Darmmunginsa <darmmunginsa@gmail.com>
2026-05-23 14:41:28 +0700

    feat: new Apps Script URL + all pending changes

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 //  แก้ไข APPS_SCRIPT_URL ให้ตรงกับ Web App URL ที่ Deploy จาก
 //  Google Apps Script ของคุณ
 // ============================================================
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzzTEPngZC4cbsZMpnRX_XTfHDzSn0jkJWjygDr1HuC92tMYHCFUdjwBlUw3zaysA51cg/exec';
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby0pOtXlwUnVaPkboN1XUX7LUo8xjAqdFZcB1e_93med40v8xNjZpykNtEvdXCo6Kti/exec';
